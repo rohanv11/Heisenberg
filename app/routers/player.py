bd36659a0969a351d03ca8e7a29b5eee53f9d7c2
Rohan Varadkar <rohanvaradkar1110@gmail.com>
2025-03-01 13:26:53 +0530

    removeing the whole python socket io code

--- app/routers/player.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/{player_id}")
-def get_player_details(player_id: str):
-    # Add player-related logic here
-    return {"player_id": player_id, "details": "Player details"}
--- app/schemas/db_models.py
+++
@@ -1,8 +0,0 @@
-
-
-class Player:
-    def __init__(self, id, name, email, password):
-        self.id = id
-        self.name = name
-        self.email = email
-        self.password = password
